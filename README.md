3e2f9d7f1ece1f1fcc79258a1767c864708c7d3f
matt-wolff <mbwolff@stanford.edu>
2025-05-18 22:42:29 -0700

    init implementation of sft.

---
+++ README.md
@@ -0,0 +1,7 @@
+## Set up environment
+
+(base) conda create -f env.yml
+
+(base) conda activate cs224r_final
+
+(cs224r_final) huggingface-cli login [use token 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 when prompted]
---
+++ sft.py
@@ -0,0 +1,60 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import argparse
+from datasets import load_dataset
+import torch
+import torch.optim as optim
+import datetime
+
+SFT_DATASET = "Asap7772/cog_behav_all_strategies"
+
+def main(args):
+    tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-0.5B")
+    model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-0.5B")
+    optimizer = optim.AdamW(model.parameters(), lr=args.lr)
+
+    train_dataset = load_dataset(SFT_DATASET, split='train')
+    # test_dataset = load_dataset(SFT_DATASET, split='test')
+
+    for epoch in range(args.num_epochs):
+        train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+        for batch in train_dataloader:
+            query_and_completion = []
+            for i in range(len(batch['query'])):
+                query_and_completion.append(batch['query'][i] + batch['completion'][i])
+            query_and_completion = tokenizer(
+                query_and_completion,
+                return_attention_mask=False,
+                padding=True,
+                return_tensors='pt'
+            )['input_ids']
+
+            output = model(query_and_completion)
+
+            loss = 0
+            for i in range(len(batch['query'])):
+                query_ids = tokenizer.encode(batch['query'][i])
+                completion_ids = tokenizer.encode(batch['completion'][i], return_tensors='pt')
+                pred_start = len(query_ids)-1
+                num_preds = len(completion_ids[0])
+                pred_logits = output['logits'][i][pred_start:pred_start+num_preds]
+                pred_probs = torch.nn.functional.softmax(pred_logits, dim=1)
+                completion_ids_col = completion_ids.reshape((-1, 1))
+
+                target_preds = torch.gather(pred_probs, 1, completion_ids_col)
+                target_preds = torch.log(target_preds)
+                loss += -torch.sum(target_preds)
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+    torch.save(model.state_dict(), f'./models/sft/epochs_{args.num_epochs}-batch_{args.batch_size}-lr_{args.lr}.pt')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_epochs', type=int, default=2)
+    parser.add_argument('--batch_size', type=int, default=2)
+    parser.add_argument('--lr', '--learning_rate', type=float, default=1e-4)
+    args = parser.parse_args()
+    main(args)
